495040554c39c86668c8bf81b690eef6c91770c2
RUDYMAZE <njockrudiger4@gmail.com>
2025-07-17 21:04:09 +0300

    added the content in the Headeer.tsx

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,7 +1,9 @@
+import Header from "@/components/layout/Header";
+
 const Post: React.FC = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <h1 className="text-7xl font-thin">Post Page</h1>
+      <Header />
     </div>
   );
 };
