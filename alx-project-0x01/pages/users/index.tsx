f4bd40c6b14459577100310efbf4ac4422c343ea
RUDYMAZE <njockrudiger4@gmail.com>
2025-07-17 21:55:29 +0300

    created the props in the interfaces directory and imported PostProps where necessary

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,18 +1,31 @@
 import Header from "@/components/layout/Header";
-const Users: React.FC = () => {
+import UserCard from "@/components/common/UserCard";
+import { UserProps } from "@/interfaces";
+
+interface UsersPageProps {
+  users: UserProps[];
+}
+
+const Users: React.FC<UsersPageProps> = ({ users }) => {
   return (
     <div className="flex flex-col h-screen">
       <Header />
+      <div className="flex flex-col items-center overflow-auto p-4">
+        {users.map((user) => (
+          <UserCard key={user.id} {...user} />
+        ))}
+      </div>
     </div>
   );
 };
+
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const posts = await response.json();
+  const users = await response.json();
 
   return {
     props: {
-      posts,
+      users,
     },
   };
 }
